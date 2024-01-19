ea79611198102f7c01aa3d833b205f76e022794c
Taiwo <taiwo1695@gmail.com>
2024-01-19 14:38:45 -0800

    puppet remake

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,5 @@
-# File: install_flask.pp
-# Purpose: Install Flask version 2.1.0 on Ubuntu 20.04 LTS
-
-package { 'python3-pip':
-  ensure => 'installed',
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => '/usr/local/bin:/usr/bin',
-  require => Package['python3-pip'],
+# Install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
