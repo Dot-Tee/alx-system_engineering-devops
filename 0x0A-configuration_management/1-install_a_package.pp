f93de8a00a920511b5500e9556f1d45fbb0a99f6
Taiwo <taiwo1695@gmail.com>
2024-01-19 14:50:56 -0800

    big

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,9 @@
+#!/usr/bin/pup
 # File: install_flask.pp
 # Purpose: Install Flask version 2.1.0 using pip3
-
 package { 'python3-pip':
   ensure => 'installed',
 }
-
 exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => '/usr/local/bin:/usr/bin',
